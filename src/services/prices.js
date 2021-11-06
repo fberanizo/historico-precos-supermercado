fdbf8626c567b765760432dd0293cfb22d51096b
Fabio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2021-11-06 07:53:12 -0300

    Adds September 2021 data
    (actually Oct. 2nd, because we postponed shopping for a few days)

--- src/services/prices.js
+++ src/services/prices.js
@@ -45,6 +45,7 @@ import j43 from '../data/2021-06-03-SUPERMERCADO-BIG-BOM-LTDA.json';
 import j44 from '../data/2021-07-05-SUPERMERCADO-BIG-BOM-LTDA.json';
 import j45 from '../data/2021-07-31-GOOD-BOM-SUPERMERCADOS-LTDA.json';
 import j46 from '../data/2021-08-31-SUPERMERCADO-BIG-BOM-LTDA.json';
+import j47 from '../data/2021-10-02-SUPERMERCADO-BIG-BOM-LTDA.json';
 
 const data = {
   '2018-07-20': j0,
@@ -94,6 +95,7 @@ const data = {
   '2021-07-05': j44,
   '2021-07-31': j45,
   '2021-08-31': j46,
+  '2021-10-02': j47,
 };
 
 export const getPrices = (regex) => {
